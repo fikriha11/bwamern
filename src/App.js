e327a66b27b1a86d4405dd9324e1669ef8253ed5
fikriha11 <fikrihaikal568@gmail.com>
2022-12-27 02:34:22 +0700

    add project bwamern

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'assets/scss/style.scss';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Hallo Dunia
+    </div>
+  );
+}
+
+export default App;
---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Button from "./index";
+import { toBeInTheDocument } from "@testing-library/jest-dom/dist/matchers";
+
+test("should not allowed click button if disabled is present", () => {
+  const { container } = render(<Button isDisabled></Button>);
+
+  // ngecek jika punya span disabled
+  expect(container.querySelector("span.disabled")).toBeInTheDocument();
+});
+
+test("should render loading/spinner", () => {
+  const { container, getByText } = render(<Button isLoading></Button>);
+
+  // ngecek jika terdaoat text loading
+  expect(getByText(/loading/i)).toBeInTheDocument();
+  // ngecek jika terdapat span
+  expect(container.querySelector("span")).toBeInTheDocument();
+});
+
+test("should render <a> tag", () => {
+  const { container } = render(<Button type="link" isExternal></Button>);
+
+  // ngecek jika terdapat span
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
+
+test("should render <Link> tag", () => {
+  const { container } = render(
+    <Router>
+      <Button href="" type="link"></Button>
+    </Router>
+  );
+
+  // ngecek jika terdapat span
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
